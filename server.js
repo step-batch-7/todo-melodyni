096fe8c144534c9997e414895aef0c5fba432ae9
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-02-02 12:02:56 +0530

    added server and its lib

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const http = require('http');
+const { createServer } = http;
+const { app } = require('./lib/handler');
 
+const main = function() {
+  const server = createServer(app.respond.bind(app));
+  server.listen(9000, () => console.log('started listening'));
+};
+
+main();
