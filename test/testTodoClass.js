e72755927b1fd5a3163732e77c791412050eb094
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-13 10:19:48 +0530

    [ragini|naveen]  added test for Todo Class method findTask

--- test/testTodoClass.js
+++ test/testTodoClass.js
@@ -28,4 +28,14 @@ describe('Todo', () => {
       assert.strictEqual(todo.generateTaskId(), '1234_2');
     });
   });
+  describe('findTask', () => {
+    it('Should find task of given TaskId if exists', () => {
+      const todo = new Todo('1234', 'TITLE', [{ taskId: '1234_1' }]);
+      assert.deepStrictEqual(todo.findTask('1234_1'), { taskId: '1234_1' });
+    });
+    it('Should find task of given TaskId if exists', () => {
+      const todo = new Todo('1234', 'TITLE', [{ taskId: '1234_1' }]);
+      assert.isUndefined(todo.findTask('1234_2'));
+    });
+  });
 });
