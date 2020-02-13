241223942ece44663697ebb7add6a70bd6604820
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-13 10:06:50 +0530

    [ragini|naveen]  added test for Todo Class method generateTaskId

--- test/testTodoClass.js
+++ test/testTodoClass.js
@@ -2,8 +2,8 @@
 const assert = require('chai').assert;
 const { TodoList, Todo, Task } = require('../lib/todoClass');
 
-describe ('Task', () => {
-  describe ('toggleStatus', () => {
+describe('Task', () => {
+  describe('toggleStatus', () => {
     it('Should set status as checked if it is unchecked', () => {
       const task = new Task('1234', 'Task1', '');
       task.toggleStatus();
@@ -15,5 +15,17 @@ describe ('Task', () => {
       assert.strictEqual(task.status, '');
     });
   });
-  
+});
+
+describe('Todo', () => {
+  describe('generateTaskId', () => {
+    it('Should generate task id as 1 for new Task', () => {
+      const todo = new Todo('1234', 'TITLE', []);
+      assert.strictEqual(todo.generateTaskId(), '1234_1');
+    });
+    it('Should generate new TaskId by incrementing the last taskId', () => {
+      const todo = new Todo('1234', 'TITLE', [{ taskId: '1234_1' }]);
+      assert.strictEqual(todo.generateTaskId(), '1234_2');
+    });
+  });
 });
