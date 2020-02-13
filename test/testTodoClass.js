c82343272a5adc00f9a8e16531309a7d7d1fc365
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-13 18:11:43 +0530

    [ragini|naveen] removed exporting classes Todo and Task and  their tests

--- test/testTodoClass.js
+++ test/testTodoClass.js
@@ -1,63 +1,6 @@
 'use strict';
 const assert = require('chai').assert;
-const { TodoList, Todo, Task } = require('../lib/todoClass');
-
-describe('Task', () => {
-  describe('toggleStatus', () => {
-    it('Should set status as checked if it is unchecked', () => {
-      const task = new Task('1234', 'Task1', 'unchecked');
-      task.toggleStatus();
-      assert.strictEqual(task.status, 'checked');
-    });
-    it('Should set status as unchecked if it is checked', () => {
-      const task = new Task('1234', 'Task1', 'checked');
-      task.toggleStatus();
-      assert.strictEqual(task.status, 'unchecked');
-    });
-  });
-});
-
-describe('Todo', () => {
-  describe('generateTaskId', () => {
-    it('Should generate task id as 1 for new Task', () => {
-      const todo = new Todo('1234', 'TITLE', []);
-      assert.strictEqual(todo.generateTaskId(), '1234_1');
-    });
-    it('Should generate new TaskId by incrementing the last taskId', () => {
-      const todo = new Todo('1234', 'TITLE', [{ taskId: '1234_1' }]);
-      assert.strictEqual(todo.generateTaskId(), '1234_2');
-    });
-  });
-  describe('findTask', () => {
-    it('Should find task of given TaskId if exists', () => {
-      const todo = new Todo('1234', 'TITLE', [{ taskId: '1234_1' }]);
-      assert.deepStrictEqual(todo.findTask('1234_1'), { taskId: '1234_1' });
-    });
-    it('Should find task of given TaskId if exists', () => {
-      const todo = new Todo('1234', 'TITLE', [{ taskId: '1234_1' }]);
-      assert.isUndefined(todo.findTask('1234_2'));
-    });
-  });
-  describe('static load', () => {
-    it('Should load and make given todo the instance of Todo class', () => {
-      const todo = { id: '1234', title: 'TITLE', tasks: [] };
-      const actual = Todo.load(todo);
-      assert.isTrue(actual instanceof Todo);
-    });
-  });
-  describe('toggleTaskStatus', () => {
-    it('Should return true if it toggles the status', () => {
-      const task = new Task('1234_1', 'task1', 'unchecked');
-      const todo = new Todo('1234', 'TITLE', [task]);
-      assert.isTrue(todo.toggleTaskStatus('1234_1'));
-    });
-    it('Should return false if given task does not exists', () => {
-      const task = new Task('1234_1', 'task1', 'unchecked');
-      const todo = new Todo('1234', 'TITLE', [task]);
-      assert.isFalse(todo.toggleTaskStatus('1234_5'));
-    });
-  });
-});
+const { TodoList } = require('../lib/todoClass');
 
 describe('TodoList', () => {
   const list = [
