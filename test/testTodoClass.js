7838cd1c6c69850012096d9d7f289658c06d276f
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-13 11:04:52 +0530

    [ragini|naveen] modified toggleTaskStatus of Task Class with Tests

--- test/testTodoClass.js
+++ test/testTodoClass.js
@@ -5,14 +5,14 @@ const { TodoList, Todo, Task } = require('../lib/todoClass');
 describe('Task', () => {
   describe('toggleStatus', () => {
     it('Should set status as checked if it is unchecked', () => {
-      const task = new Task('1234', 'Task1', '');
+      const task = new Task('1234', 'Task1', 'unchecked');
       task.toggleStatus();
       assert.strictEqual(task.status, 'checked');
     });
     it('Should set status as unchecked if it is checked', () => {
       const task = new Task('1234', 'Task1', 'checked');
       task.toggleStatus();
-      assert.strictEqual(task.status, '');
+      assert.strictEqual(task.status, 'unchecked');
     });
   });
 });
