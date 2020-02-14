85776d22905021619630d409be38186dec71b716
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-14 13:02:07 +0530

    [ragini|naveen] moved all eventHandlers to eventHandlers.js and renamed todoApp.js to homePage.js

---
+++ public/js/eventHandlers.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const sendXMLRequest = function(reqMethod, url, callBack, data) {
+  const STATUS_CODES = { OK: 200 };
+  const request = new XMLHttpRequest();
+  const methods = ['POST', 'DELETE'];
+  request.onload = function() {
+    if (this.status === STATUS_CODES.OK) {
+      callBack(this.responseText);
+    }
+  };
+  request.open(reqMethod, url);
+  if (methods.some(method => method === reqMethod)) {
+    request.setRequestHeader('Content-Type', 'application/json');
+  }
+  request.send(data);
+};
+
+const deleteTask = function(taskId) {
+  const message = JSON.stringify({ taskId });
+  sendXMLRequest('DELETE', '/deleteTask', displayTasks, message);
+};
+
+const deleteTodo = function(todoId) {
+  const message = JSON.stringify({ todoId });
+  sendXMLRequest('DELETE', '/deleteTodo', displayTodos, message);
+};
+
+const toggleTaskStatus = function(taskId) {
+  const message = JSON.stringify({ taskId });
+  sendXMLRequest('POST', '/toggleTaskStatus', () => {}, message);
+};
+
+const getValue = element => element.value;
+
+const saveTodo = function() {
+  const title = document.querySelector('#todoTitle').value;
+  const taskElements = Array.from(document.querySelectorAll('.taskInput'));
+  const tasks = taskElements.map(getValue);
+  const todoContent = JSON.stringify({ title, tasks });
+  sendXMLRequest('POST', '/postNewTodos', loadHomePage, todoContent);
+};
+
+const getIdAndValue = element => {
+  const taskId = element.id;
+  const task = element.value;
+  return { taskId, task };
+};
+
+const updateTodo = todoId => {
+  const title = document.querySelector('#todoTitle').value;
+  const taskElements = Array.from(document.querySelectorAll('.task'));
+  const tasks = taskElements.map(getIdAndValue);
+  const todoContent = JSON.stringify({ title, tasks, todoId });
+  sendXMLRequest('POST', '/updateTodo', loadHomePage, todoContent);
+};
