0943580d9956bbc30865e5286db0cf77bc7e8dbb
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-02-20 12:33:20 +0530

    [ragini|naveen] changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { app } = require('./lib/routes');
 
 const main = function() {
-  const port = 9000;
+  const port = process.env.PORT || 9000;
   app.listen(port, () => {
     process.stderr.write(`started listening to port:${port}`);
   });
