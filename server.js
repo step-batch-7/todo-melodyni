685fc89e4f6006e51b9d93397cebd153a5177ccc
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-02-20 12:50:43 +0530

    [ragini|naveen] added setup for dataBase directory

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
 const { app } = require('./lib/routes');
 
+const setUpDataBase = function() {
+  const DATA_PATH = `${__dirname}/dataBase`;
+  if (!fs.existsSync(DATA_PATH)) {
+    fs.mkdirSync(DATA_PATH);
+  }
+};
+
 const main = function() {
+  setUpDataBase();
   const port = process.env.PORT || 9000;
   app.listen(port, () => {
     process.stderr.write(`started listening to port:${port}`);
